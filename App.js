008d5376428b8527effea474aa2ea20008a5afc4
Luigi-Recco <luigireccopinheiro@gmail.com>
2024-06-03 22:32:22 -0300

    oi

--- App.js
+++ App.js
@@ -1,152 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StyleSheet,Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./Screen/Home";
+import LoginScreen from "./Screen/Login";
 
-import { StyleSheet, Text, SafeAreaView, View, Pressable, TextInput, FlatList, ActivityIndicator} from 'react-native';
+        const Stack = createNativeStackNavigator();
 
-import Diario from './pages/Diario';
+export default functionApp()
+{
+    return(
 
-import { AntDesign } from '@expo/vector-icons';
-
-import { useState,useEffect } from 'react';
-
-import {db, collection, addDoc, getDocs,deleteDoc,doc} from './firebase/firebase'
-
-
-export default function App() {
- 
- 
-    const [title,setTitle] = useState("");
-    const [mostraDiario,setMostraDiario] = useState([]);
- 
-    //cadastra dados
-    const addItensDiario = async() => {
-      try {
-        const docRef = await addDoc(collection(db, "diario"), {
-          title: title,
-          isChecked:false,
-
-        });
-        console.log("Document written with ID: ", docRef.id);
-        setTitle("");
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
-        getmostrarDiarios();
-    };
-  
-    //le os dados
-    const getmostrarDiarios = async() => {
-      const querySnapshot = await getDocs(collection(db, "diario"));
-      setMostraDiario(querySnapshot.docs.map((doc)=>({...doc.data(),id: doc.id})));
-    };
-
-    const deleteDiariocolection = async() =>{
-      const querySnapshot = await getDocs(collection(db, "diario"));
-     
-      querySnapshot.docs.map((item)=> deleteDoc(doc(db, "diario", item.id)));
-      getmostrarDiarios();
-
-    }
-
-    useEffect(() => {
-
-      getmostrarDiarios();
-
-    }, []);
-
-    return (
-    <SafeAreaView style={styles.container}>
-          
-          <View style={styles.header}>
-
-              <Text style={styles.heading}>Diario Pessoal</Text>
-
-
-              <Text style={styles.noOfItens}> {mostraDiario.length} </Text>
-
-
-
-              <Pressable onPress={deleteDiariocolection}>
-            
-            <AntDesign name="delete" size={24} color="black" />
-           
-            </Pressable>
-
-          </View>
-          {
-              mostraDiario.length > 0 ?(
-            
-            <FlatList
-            
-            data={mostraDiario}
-            
-            renderItem={({item})=>(
-             <Diario
-              title={item.title} 
-              isChecked={item.isChecked}
-              id={item.id}
-              getmostrarDiarios={getmostrarDiarios}/>
-            )}
-            keyExtractor={item => item.id}
-              />
-            ):(
-            
-              <ActivityIndicator/>
-             )}
-
-
-           
-            <TextInput placeholder="Escreva suas lembranças"
-             style={styles.input}
-             value ={title}
-             onChangeText={(text)=> setTitle(text)}
-             onSubmitEditing={addItensDiario}
-             />
-            
-            <StatusBar style="auto" />
-
-
-    </SafeAreaView>
-  );
+        <NavigationContainer>
+                <Stack.Navigator>
+                      <Stack.Screen  name="Home" component={HomeScreen}/>
+                      <Stack.Screen  options={{headesShown: false}} name="Login" component={LoginScreen}/>
+                 </Stack.Navigator>
+         </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-
-  },
-  header:{
-      flexDirection:'row',
-      backgroundColor:'#fff',
-      width:'%90',
-      alignSelf:'center',
-      padding:10,
-      justifyContent:'space-between',
-      alignItems:'center',
-      marginBottom:10,
-      marginTop:20
-      
-  },
-  heading:{
-      fontSize:30,
-      fontWeight:'500',
-      flex:1,
-      color:'#4169E1',
-  },
-  noOfItens:{
-      fontSize:30,
-      fontWeight:'500',
-      marginRight:20,
-      color:'#4169E1',
-  },
-  input:{
-    backgroundColor:'#40E0D0',
-    padding:10,
-    fontSize:17,
-    width:'%90',
-    alignSelf:'center',
-    borderRadius:10,
-    marginTop: 'auto',
-    marginBottom:10
-  }
-});
--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,25 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
-import { getFirestore, collection, addDoc, getDocs, updateDoc, doc, deleteDoc } from "firebase/firestore";
-
-
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0jsuEqCC-KYIz5RdwOPjx6D1snzuOYag",
-  authDomain: "meu-diario-de-react.firebaseapp.com",
-  projectId: "meu-diario-de-react",
-  storageBucket: "meu-diario-de-react.appspot.com",
-  messagingSenderId: "759259239102",
-  appId: "1:759259239102:web:a0bf822763ba8f1ed4c4ef"
+  apiKey: "AIzaSyBnuFHZbC6oeOMgZdoTG_HL4cbs9rWrIpU",
+  authDomain: "app-filmes-legais.firebaseapp.com",
+  projectId: "app-filmes-legais",
+  storageBucket: "app-filmes-legais.appspot.com",
+  messagingSenderId: "310695462707",
+  appId: "1:310695462707:web:6c84c7c468c567eec3e046"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export {app,db,getFirestore, collection, addDoc, getDocs, updateDoc, doc, deleteDoc};
